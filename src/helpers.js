d098a658fb2052803a9b846b7ea6ad135cb110f4
freddiesmith <freddie.smith@sentium-consulting.com>
2022-10-03 18:22:02 +0100

    allow alternate options for english

--- src/helpers.js
+++ src/helpers.js
@@ -57,34 +57,41 @@ export const shuffle = (array) => {
     };
 
     const defarticles = ["τό","το","ὁ","ἡ"];
-
-    if (langs[langs.selectedLang]==="greek") {
-        let wordType = "unknown";
-        const splitAnswer = answer.split(",");
-        if (splitAnswer.length===3) {
-            if (defarticles.includes(splitAnswer[2])) {
-                wordType = "noun";
-            } else {
-                wordType = "adj";
+    const splitAnswer = answer.split(",");
+    const splitTest = test.split(",");
+    switch (langs[langs.selectedLang]) {
+        case "greek" :
+            let wordType = "unknown";
+            if (splitAnswer.length===3) {
+                if (defarticles.includes(splitAnswer[2])) {
+                    wordType = "noun";
+                } else {
+                    wordType = "adj";
+                };
             };
-        };
-        if (splitAnswer.length===2) wordType = "adj";
-        const splitTest = test.split(",");
-        console.log("splits", splitTest, splitAnswer, wordType)
-        if (splitTest.length!==splitAnswer.length) {
-            switch (wordType) {
-                case "noun":
-                    if (splitTest.length===1) answer = splitAnswer[0];
-                    break;
-                case "adj":
-                    if (splitTest.length===1) answer = splitAnswer[0];
-                    console.log("answer",answer)
-                    break;
-                case "verb":
-                    break;
-                default: break;
+            if (splitAnswer.length===2) wordType = "adj";
+            if (splitTest.length!==splitAnswer.length) {
+                switch (wordType) {
+                    case "noun":
+                        if (splitTest.length===1) answer = splitAnswer[0];
+                        break;
+                    case "adj":
+                        if (splitTest.length===1) answer = splitAnswer[0];
+                        console.log("answer",answer)
+                        break;
+                    case "verb":
+                        break;
+                    default: break;
+                };
             };
-        };
+            break;
+        case "english":
+            const result = splitTest.reduce((acc, cur) => {
+                if (!splitAnswer.includes(cur)) return false;
+                return true;
+            },true);
+            if (result) answer = test;
+            break;
     };
     
     
